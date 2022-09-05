360787d672c64b7c3d6afc05a325658c11e28a4c
TwinTitan <abdulla.al.shammeri@gmail.com>
2022-09-05 20:13:30 +0300

    cw1

--- cw1/cw1/ContentView.swift
+++ cw1/cw1/ContentView.swift
@@ -9,40 +9,54 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack{ Color.purple
+        ZStack{ Image("bg")
+                .resizable(resizingMode: .stretch)
+                .scaledToFill()
+                .opacity(1)
         VStack{
             
             
         Text("Hello my name is abdallah")
                 .foregroundColor(Color.white)
                 .font(.largeTitle)
+                .opacity(0.8)
             
             Text("I am 26 Years old")
                 .foregroundColor(Color.white)
                 .font(.largeTitle)
+                .opacity(0.8)
     
             Text("I am learning SwiftUI now")
                 .foregroundColor(Color.white)
                 .font(.largeTitle)
+                .opacity(0.8)
             
             
             HStack{Spacer()
             Image(systemName: "heart.fill")
                     .foregroundColor(Color.white)
                     .font(.largeTitle)
+                    .opacity(0.8)
                 Spacer()
+                
             Image(systemName: "gear")
                     .foregroundColor(Color.white)
                     .font(.largeTitle)
+                    .opacity(0.8)
                 Spacer()
             Image(systemName: "trash.fill")
                     .foregroundColor(Color.white)
                     .font(.largeTitle)
+                .opacity(0.8)
                 Spacer()
+                
+                
+                
             }
             
         }
                 }
+        .ignoresSafeArea()
                 }
                 }
 
